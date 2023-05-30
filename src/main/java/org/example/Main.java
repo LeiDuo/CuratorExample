e8a64e557ea207e703d3fd3d2cb8a1cd8cdcd1f0
LeiDuo <1242853262@qq.com>
2023-05-30 14:34:04 +0800

    确定需要导出数据的点位

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
 import org.example.client.CuratorClient;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Scanner;
+
+import static java.lang.System.in;
 
 /**
  * main.
@@ -17,7 +20,7 @@ public class Main {
         // 这里直接监听根节点，实际上几个分布式任务的节点应该移出监听范围
         curatorClient.addCache("/", (cache, cachePaths, event) -> {
             log.info("基于{}节点的监听触发", "/");
-            log.info("节点发生更改: {}\n更改类型: {}", event.getData().getPath(), event.getType());
+            log.info("节点发生更改: {}\t更改类型: {}", event.getData().getPath(), event.getType());
             HashSet<String> paths = cachePaths.get("/");
             // cache刚建立时，内部数据为空，curator从zk读取数据，并触发监听，监听事件为add类型.
             if (event.getType() == TreeCacheEvent.Type.NODE_ADDED) {
@@ -30,15 +33,25 @@ public class Main {
                         paths.add(event.getData().getPath());
                     } else {
                         log.info("删除{}节点后又新增了该节点", event.getData().getPath());
+                        // 模拟数据的导出
+                        for (String path : paths) {
+                            cache.get(path).ifPresent(each -> log.warn(path + ":\n" + new String(each.getData(), StandardCharsets.UTF_8)));
+                        }
                     }
                 }
+                return;
+            }
+            //模拟数据的导出
+            for (String path : paths) {
+                cache.get(path).ifPresent(each -> log.warn(path + ":\n" + new String(each.getData(), StandardCharsets.UTF_8)));
             }
         });
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(in);
         while (sc.hasNext()) {
             if ("exit".equalsIgnoreCase(sc.next())) {
                 return;
             }
         }
     }
+
 }
